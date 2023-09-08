cf413b847cf168dadddb1b6e86f03c3d804ea037
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-09-08 19:11:03 +0200

    update secret key config

--- cryptics_core_1/settings.py
+++ cryptics_core_1/settings.py
@@ -151,8 +151,8 @@ EMAIL_MULTI_USER = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
-EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
