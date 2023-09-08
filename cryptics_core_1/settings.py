a6b715c4e3bce9fceb1603f9737b139b9839e2f6
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-09-08 17:45:53 +0200

    integreting extenal database

--- cryptics_core_1/settings.py
+++ cryptics_core_1/settings.py
@@ -81,9 +81,14 @@ WSGI_APPLICATION = 'cryptics_core_1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
 DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-    }
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+    )
+}
 
 
 # Password validation
