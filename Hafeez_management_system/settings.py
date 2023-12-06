919eeff2a26cea17d5399c533891f3bf91c4bbc1
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-12-06 22:38:38 +0200

    updating data virtualization and other frontend componets

--- Hafeez_management_system/settings.py
+++ Hafeez_management_system/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 MAIN_DOMAIN = os.environ.get('MAIN_DOMAIN')
 SUBDOMAIN_NAME = os.environ.get('SUBDOMAIN_NAME')
