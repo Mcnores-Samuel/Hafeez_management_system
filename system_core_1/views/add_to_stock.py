42bba3861c8ff890fb1a65b4d7658e6ae4cc516d
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-04-10 15:55:56 +0200

    official update of the bulk additional of stock

--- system_core_1/views/add_to_stock.py
+++ system_core_1/views/add_to_stock.py
@@ -4,9 +4,16 @@ from ..forms.add_to_stock import AddToStockForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from ..models.main_storage import MainStorage
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import Group
+from ..models.user_profile import UserProfile
+from django.utils import timezone
+from django.http import JsonResponse
+import json
 
 
 @login_required
+@csrf_exempt
 def add_to_stock(request):
     """This view function is responsible for handling the addition of phones to stock.
 
@@ -28,7 +35,8 @@ def add_to_stock(request):
     Agents access this view to add phones to stock.
     It ensures that only agents are able to access this view.
     """
-    if request.user.is_authenticated and request.user.groups.filter(name='staff_members').exists():
+    if (request.user.is_authenticated and request.user.groups.filter(name='staff_members').exists()
+        or request.user.is_staff and request.user.is_superuser):
         user = request.user
         data = MainStorage.objects.all()
         phone_names = set()
@@ -37,29 +45,33 @@ def add_to_stock(request):
                 phone_names.add(phone.name)
         sorted_phone_list = sorted(list(phone_names))
         if request.method == 'POST':
-            form = AddToStockForm(request.POST, user=user)
-            if form.is_valid():
-                form.process_data()
-                messages.success(request, 'Phone added to stock successfully.')
-                return redirect('add_to_stock')
-        else:
-            form = AddToStockForm(user=user)
-        return render(request, 'users/staff_sites/add_to_stock.html', {'form': form, 'phone_names': sorted_phone_list})
-    elif request.user.is_staff and request.user.is_superuser:
-        user = request.user
-        data = MainStorage.objects.all()
-        phone_names = set()
-        for phone in data:
-            if phone.name is not None:
-                phone_names.add(phone.name)
-        sorted_phone_list = sorted(list(phone_names))
-        if request.method == 'POST':
-            form = AddToStockForm(request.POST, user=user)
-            if form.is_valid():
-                form.process_data()
-                messages.success(request, 'Phone added to stock successfully.')
-                return redirect('add_to_stock')
-        else:
-            form = AddToStockForm(user=user)
-        return render(request, 'users/admin_sites/add_to_stock.html', {'form': form, 'phone_names': sorted_phone_list})
-    return render(request, '403.html')
+            data = json.loads(request.POST.get('data'))
+            name = request.POST.get('name')
+            cost_price = request.POST.get('cost_price')
+            supplier = request.POST.get('supplier')
+            instance = MainStorage.objects.filter(name=name).first()
+            main_shop_staff = Group.objects.get(name='main_shop')
+            representatives = UserProfile.objects.filter(groups=main_shop_staff)
+            agent = representatives.first()
+            already_exists = []
+            for item in data:
+                try:
+                    MainStorage.objects.create(
+                        device_imei=item[0], device_imei_2=item[1], name=name,
+                        phone_type=instance.phone_type, category=instance.category,
+                        spec=instance.spec, screen_size=instance.screen_size,
+                        battery=instance.battery, camera=instance.camera, os=instance.os,
+                        in_stock=True, sales_type='##', contract_no='##',
+                        entry_date=timezone.now(), stock_out_date=timezone.now(),
+                        collected_on=timezone.now(), assigned=True, sold=False,
+                        paid=False, cost=cost_price, price=0.00, agent=agent,
+                        recieved=True, on_display=False, pending=False, missing=False,
+                        supplier=supplier, faulty=False, assigned_from='Hafeez Enterprises',
+                        updated_by=user.username, comment='##'
+                    )
+                except Exception as e:
+                    already_exists.append(item)
+            return JsonResponse({'status': 200, 'data': already_exists})
+    if request.user.is_staff and request.user.is_superuser:
+        return render(request, 'users/admin_sites/add_to_stock.html', {'phone_names': sorted_phone_list})
+    return render(request, 'users/staff_sites/add_to_stock.html', {'phone_names': sorted_phone_list})
